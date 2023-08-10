083550b4ed7d3eba99df0bed62dd3c3817ff3954
Wes <wesley.has@outlook.com>
2023-08-10 08:39:57 -0600

    added past, present and future functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,8 @@
+window.onload = function() {
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,8 +21,73 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
 
+// var now = dayjs();
+// console.log(now);
+
+//adds current date
+var dateDisplayElement = document.getElementById("dateDisplay");
+var currentDate = dayjs().format("dddd, MMMM DD - h:mm A");
+dateDisplayElement.textContent = currentDate;
+
+//check what is the current time and
+//if the current time is past then remove current class and add past class.
+//else if the current time is present then remove current class and add present class. 
+//else if the current is future then remove current class and add future class.
+
+/* var textHourElements = document.querySelectorAll('#get-hour');
+
+textHourElements.forEach(function (textHourElement) {
+  var textHour = textHourElement.textContent;
+  var textTimeInteger = parseInt(textHour);
+  // var now = dayjs();
+  // var currentHour = now.hour();
+  var currentHour = dayjs().hour();
+
+  // console.log(typeof currentHour);
+  // console.log(textTimeInteger);
+  // console.log(dayjs(textTimeInteger));
+  console.log(currentHour);
+
+  if (textTimeInteger === currentHour) {
+    $('.time-block').removeClass('past present future');
+    $('.time-block').addClass('present');
+  } else if (textTimeInteger < currentHour) {
+    $('.time-block').removeClass('past present future');
+    $('.time-block').addClass('past');
+  } else if (textTimeInteger > currentHour) {
+    $('.time-block').removeClass('past present future');
+    $('.time-block').addClass('future');  
+  }
+
+}); */
+
+var timeBlockElements = document.querySelectorAll('.time-block');
+
+  var hourNumbers = Array.from(timeBlockElements).map(function(element) {
+    var id = $(element).attr('id');
+    var numericPart = id.split('-')[1];
+    return parseInt(numericPart);
+  });
+
+  var currentHour = dayjs().hour();
+
+  // Loop through time block elements and apply classes based on comparison
+  timeBlockElements.forEach(function (element, index) {
+    var hourNumber = hourNumbers[index];
+    var $element = $(element);
+
+    $element.removeClass('past present future'); // Clear previous classes
+
+    if (hourNumber === currentHour) {
+      $element.addClass('present');
+    } else if (hourNumber < currentHour) {
+      $element.addClass('past');
+    } else if (hourNumber > currentHour) {
+      $element.addClass('future');
+    }
+  });
 
 // var textInput = JSON.parse(localStorage.getItem('inputcontent')) || ['', '', '', '', '', '', '', '', ''];
 
@@ -42,4 +108,4 @@ $( ".btn" ).each(function( i ) {
   } );
 
 });
-// alert(localStorage.getItem('inputcontent'));
+};
